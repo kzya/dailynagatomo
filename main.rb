ff02513a438d0e54b00e9c715a58f8a9144d3c40
kzya <nagatomo10311031@gmail.com>
2016-06-26 03:27:23 +0900

    add edge,line_detection,output_show

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require 'opencv'
 include OpenCV
 load './lib/edge_detection.rb'
 
-detection = EdgeDetection.new()
+detection = Image.new()
 
 detection.edge
+detection.line_detection
+detection.output_show
