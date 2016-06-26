4c4acc4c7829cc5270cb4470cccadc5ad24d2601
kzya <nagatomo10311031@gmail.com>
2016-06-26 14:14:20 +0900

    add line_detection line_drow

--- lib/edge_detection.rb
+++ lib/edge_detection.rb
@@ -1,21 +1,41 @@
-class EdgeDetection
-  def edge
+class Image
 
-    output_window = GUI::Window.new('Output')
-
-    input_image = CvMat.load("./utils/images/many_pieces.jpg")
+  def initialize
+    @output_window = GUI::Window.new('Output')
 
     begin
+    input_image = CvMat.load(ARGV[0], CV_LOAD_IMAGE_GRAYSCALE)
     rescue
       puts '開けませんでした'
       exit
     end
 
-    output_image = input_image.BGR2GRAY.canny(70,70)
+    @resize_image = input_image.resize(CvSize.new(800,600))
+  end
 
-    output_window.show output_image
+  def edge
+    gaus_image = @resize_image.smooth(CV_GAUSSIAN)
+    gaus_image, th = gaus_image.threshold(0,255, CV_THRESH_OTSU)
+    @edge_detection = gaus_image.canny(50,50)
+  end
+
+  def line_detection
+    @hough = @edge_detection.hough_lines(CV_HOUGH_PROBABILISTIC, 1, Math::PI / 300, 10, 10, 10)
+  end
+
+  def line_draw
+    @result = @edge_detection.GRAY2BGR
+    @hough.each do |points|
+      @result.line!(*points, color:CvColor::Yellow, thickness:2, line_type: :aa)
+    end
+  end
+
+  def output_show
+    @output_window.show @edge_detection
+    @output_window.show @result
 
     GUI::wait_key
     GUI::Window.destroy_all
   end
+
 end
--- main.rb
+++ main.rb
@@ -2,8 +2,10 @@ require 'opencv'
 include OpenCV
 load './lib/edge_detection.rb'
 
+
 detection = Image.new()
 
 detection.edge
 detection.line_detection
+detection.line_draw
 detection.output_show
