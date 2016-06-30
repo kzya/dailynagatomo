d519ef50bcfe0b716cea8737232c597cd5b54463
kzya <nagatomo10311031@gmail.com>
2016-06-30 09:16:44 +0900

    add hough-line class

--- lib/edge_detection.rb
+++ lib/edge_detection.rb
@@ -1,10 +1,10 @@
 class Image
 
-  def initialize
+  def initialize(image_path)
     @output_window = GUI::Window.new('Output')
 
+    input_image = CvMat.load(image_path, CV_LOAD_IMAGE_GRAYSCALE)
     begin
-    input_image = CvMat.load(ARGV[0], CV_LOAD_IMAGE_GRAYSCALE)
     rescue
       puts '開けませんでした'
       exit
@@ -15,24 +15,23 @@ class Image
 
   def edge
     gaus_image = @resize_image.smooth(CV_GAUSSIAN)
-    gaus_image, th = gaus_image.threshold(0,255, CV_THRESH_OTSU)
-    @edge_detection = gaus_image.canny(50,50)
-  end
+  # gaus_image, th = gaus_image.threshold(0,255, CV_THRESH_OTSU)
+    @edge_detection = gaus_image.canny(40,40)
+ end
+
 
   def line_detection
     @hough = @edge_detection.hough_lines(CV_HOUGH_PROBABILISTIC, 1, Math::PI / 300, 10, 10, 10)
-  end
-
-  def line_draw
     @result = @edge_detection.GRAY2BGR
     @hough.each do |points|
       @result.line!(*points, color:CvColor::Yellow, thickness:2, line_type: :aa)
     end
   end
 
+
   def output_show
     @output_window.show @edge_detection
-    @output_window.show @result
+   @output_window.show @result
 
     GUI::wait_key
     GUI::Window.destroy_all
